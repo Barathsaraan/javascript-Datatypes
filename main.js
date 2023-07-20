3672718cb867b70097bf4376ff181e67c01b7e61
Barathsaraan <saraan.barath@gmail.com>
2023-07-20 17:01:07 +0530

    add

---
+++ main.js
@@ -0,0 +1,29 @@
+let name="Barathsaraan";
+let profession='Developer';
+let native=`PCM`;
+console.log(typeof native)  
+console.log(name,profession,native);
+
+// Number
+
+let num1=20;
+let num2=2.5;
+console.log(num1,num2);
+
+// Bigint
+
+let lng1=1234456789567895678n
+let lng2=1289567834456789567n
+let result=lng1+lng2
+console.log(result)
+
+
+// boolean
+
+confirm();
+
+
+// undefined
+
+let abc;
+console.log(abc);
